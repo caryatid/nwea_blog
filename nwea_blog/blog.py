5a959360ada7c92675e9b548bfa4ca925adc2b13
Dave Setchell <setchell.dave@gmail.com>
2017-02-07 09:35:48 -0800

    basic fucntioning

--- nwea_blog/blog.py
+++ nwea_blog/blog.py
@@ -1,30 +1,55 @@
+import codecs
 import json
 import sqlite3
 
-# does sqlite allow `with` context for cursors?
-conn = sqlite3.connection('blog.db')
 
-def post_stream():
-    pass
+INSERT_POST_SQL='''
+INSERT INTO posts (title, body) VALUES (?,?)
+'''.strip()
 
-def get_stream():
-    pass
 
-# non stream GET/POST?
+LIST_POSTS_SQL='''
+SELECT post_id, title, body FROM posts;
+'''.strip()
+
+
+def post(environ, c):
+    reader = codecs.getreader('utf-8')
+    p_data = json.load(reader(environ['wsgi.input']))
+    c.execute(INSERT_POST_SQL, (p_data['title'], p_data['body']))
+    return json.dumps({'body': 'Inserted blog post: {}'.format(p_data['title'])})
+
+
+def posts(c):
+    r_names = ['post_id', 'title', 'body']  # names from sql columns?
+    retval = []
+    for r in c.execute(LIST_POSTS_SQL):
+        retval.append({k: v for k, v in zip(r_names, r)})
+    return json.dumps(retval)
+
 
 def route(environ, endpoint):
+    status = '200 OK'
     cmd = endpoint[0]
-    if cmd == 'post':
-        pass
-    elif cmd == 'posts':
-    else:
-        status = '404 Not found'
-        response = ['Nothing here, friends']
-    pass
+    with sqlite3.connect('blog.db') as conn:
+        if cmd == 'post':
+            response = post(environ, conn)
+        elif cmd == 'posts':
+            response = posts(conn)
+        else:
+            status = '404 Not found'
+            response = json.dumps({'body': 'Nothing here, friends'})
+    return (status, response)
 
 
 def application(environ, start_response):
-    response_headers = [('Content-type', 'application/json')]
+    response_headers = [('Content-Type', 'application/json')]
     l = [x.lower() for x in environ['PATH_INFO'][1:].split('/')]
     l = ['posts'] if not l else l  # list incase of future endpoints
-    pass
+    try:
+        status, response = route(environ, l)
+    except Exception as e:
+        status = '500 Unknown server error'
+        response = json.dumps({'body': e})
+    start_response(status, response_headers)
+    return codecs.encode(response)
